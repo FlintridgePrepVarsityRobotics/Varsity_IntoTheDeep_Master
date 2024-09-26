39bd03343f4c8e18136bf338e0b64e4bb83c86e9
cksakai <cksakai3000@gmail.com>
2024-09-26 15:26:24 -0700

    added hwmap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/HWMapBasic.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode.Projects;
+
+
+import android.widget.Button;
+
+import com.google.blocks.ftcrobotcontroller.util.ProjectsUtil;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+
+public class HWMapBasic extends Project {
+    public DcMotor fLeftWheel = null;
+    //2
+    public DcMotor fRightWheel = null;
+    //3
+    public DcMotor bLeftWheel = null;
+    //1
+    public DcMotor bRightWheel = null;
+    //0
+
+
+
+
+
+    public WebcamName camera = null;
+
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        // Get motors from hardware map
+        fLeftWheel = hwMap.dcMotor.get("FrontLeft");
+        fRightWheel = hwMap.dcMotor.get("FrontRight");
+        bLeftWheel = hwMap.dcMotor.get("BackLeft");
+        bRightWheel = hwMap.dcMotor.get("BackRight");
+
+
+
+//        oClaw = hwMap.servo.get("oClaw");
+        //wrist = hwMap.servo.get("wrist");
+        //wrist = hwMap.dcMotor.get("wrist");
+
+
+
+        // Set Direction
+        fRightWheel.setDirection(DcMotor.Direction.FORWARD);
+        fLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        bRightWheel.setDirection(DcMotor.Direction.FORWARD);
+        bLeftWheel.setDirection(DcMotor.Direction.REVERSE);
+        //wrist.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set run mode
+        fRightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fLeftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bRightWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bLeftWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //wrist.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Set brakes
+        fRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bLeftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //wrist.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Get webcam from hardware map
+        // camera = hwMap.get(WebcamName.class, "webcam");
+
+        Stop();
+    }
+    public void Stop(){
+        fRightWheel.setPower(0);
+        fLeftWheel.setPower(0);
+        bRightWheel.setPower(0);
+        bLeftWheel.setPower(0);
+
+
+//        slide.setPower(0);
+        //wrist.setPower(0);
+    }
+}
